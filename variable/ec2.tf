b8ac19fab108e332b826f062a6ad2eb8ac9fc986
theppasiva <theppasiva@gmail.com>
2024-01-27 13:41:47 +0530

    variable

--- variable/ec2.tf
+++ variable/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  ami           = "var.ami_id" #devops practice
-  instance_type = "var.instance_type"
+  ami           = var.ami_id #devops practice
+  instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.roboshop-all.id] #this means list
 
-  tags = "var.tags"
+  tags = var.tags
   
 }
