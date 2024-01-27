38411598df3bc9cecb9e1579b63dc5beed7a1063
theppasiva <theppasiva@gmail.com>
2024-01-27 11:49:35 +0530

    terraform

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "web" {
   ami           = "ami-0f3c7d07486cad139" #devops practice
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.roboshop-all.id] #this means list
+  vpc_security_group_ids = [aws_security_group.roboshop-all-new.id] #this means list
 
   tags = {
     Name = "Helloterraform"
   }
 }
 
-resource "aws_security_group" "roboshop-all" { #this is for terraform name
+resource "aws_security_group" "roboshop-all-new" { #this is for terraform name
     name        = "var.sg-name" #this is aws name
     description = "var.sg-description"
     #vpc_id      = aws_vpc.main.id
